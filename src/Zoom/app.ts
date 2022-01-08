bc5b2ba86f48507302be6f22693552c4ebc3f1b7
mati <mattavzzz@gmail.com>
2022-01-08 16:04:24 +0100

    second exercise

---
+++ src/Zoom/app.ts
@@ -0,0 +1,41 @@
+const imgUrl =
+  "https://cdn.pixabay.com/photo/2016/06/20/21/46/sport-shoes-1470061_960_720.jpg";
+
+const imageContainer = document.querySelector(".imageContainer");
+const container = document.querySelector(".container");
+
+const handleMouseOver = (event: Event) => {
+  const newImageContainer = document.createElement("div");
+  const newImage = document.createElement("img");
+  newImage.src = imgUrl;
+  newImage.classList.add("newImage");
+  newImageContainer.classList.add("zoomImageContainer");
+  newImageContainer.appendChild(newImage);
+  imageContainer?.appendChild(newImageContainer);
+};
+
+const handleMouseOut = () => {
+  const child = document.querySelector(".zoomImageContainer");
+  child?.remove();
+};
+
+const handleMouseMove = (event: Event) => {
+  const newImage: HTMLLIElement = document.querySelector(
+    ".newImage"
+  ) as HTMLImageElement;
+
+  const XYZ = imageContainer?.getBoundingClientRect();
+
+  //newImage.style.top =
+
+  console.log(event.clientX - XYZ.left);
+  newImage.style.left = `-${event.clientX - XYZ.left}px`;
+
+  //console.log(XYZ.left);
+};
+
+imageContainer?.addEventListener("mouseover", handleMouseOver);
+imageContainer?.addEventListener("mouseout", handleMouseOut);
+imageContainer?.addEventListener("mousemove", (event) =>
+  handleMouseMove(event)
+);
