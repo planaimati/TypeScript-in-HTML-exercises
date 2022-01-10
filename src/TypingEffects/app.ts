767afe2adf71dbe469b6140d04c4f0bc21abc6b0
mati <mattavzzz@gmail.com>
2022-01-10 12:57:41 +0100

    third exercise

--- src/TypingEffects/app.ts
+++ src/TypingEffects/app.ts
@@ -2,6 +2,7 @@ class Typed {
   constructor(private textElement: HTMLElement, private speed: number) {}
 
   text = this.textElement.innerText;
+  nexText = "";
   textArr = this.text.split("");
   counter = 0;
 
@@ -9,23 +10,47 @@ class Typed {
     this.textElement.innerText = " ";
   }
 
-  writeText() {
-    console.log(this.textArr);
+  deleteText() {
+    const deleteLetters = setInterval(() => {
+      if (this.counter > -this.text.length - 1) {
+        
 
-    const addLetters = setInterval(() => {
-      if (this.counter < this.textArr.length) {
-        this.textElement.innerText =
-          this.textElement.innerText + this.textArr[this.counter];
-        this.counter++;
+        
+
+        this.textElement.innerText.slice(0, this.counter);
+
+        this.counter--;
       } else {
-        clearInterval(addLetters);
+        clearInterval(deleteLetters);
         return;
       }
     }, 100);
+  }
+
+  writeText() {
+    const addLetters = setInterval(() => {
+      if (this.counter < this.textArr.length) {
+        this.counter++;
+        this.textElement.innerText = this.text.slice(0, this.counter);
+        
+
+        this.nexText = this.text.slice(0, this.counter);
+        
+      } else {
+        
+        clearInterval(addLetters);
+        this.counter = 0;
+        return;
+      }
+    }, 100);
+
+    setTimeout(() => {
+      this.deleteText();
+    }, 3000);
   }
 }
 
 const typed = new Typed(document.querySelector(".heading") as HTMLElement, 5);
 
-typed.writeText();
 typed.clearText();
+typed.writeText();
